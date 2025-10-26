d93a01db2562fbb39e4736c03817aadca553760f
iimukminov <iimukminov06@gmail.com>
2025-10-26 14:59:40 +0300

    Add HikariCP to work with database

---
+++ src/main/java/ru/kpfu/itis/mukminov/listener/AppContextListener.java
@@ -0,0 +1,48 @@
+package ru.kpfu.itis.mukminov.listener;
+
+import javax.servlet.ServletContextListener;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import ru.kpfu.itis.mukminov.dao.ClientDao;
+import ru.kpfu.itis.mukminov.dao.EmployeeDao;
+import ru.kpfu.itis.mukminov.dao.impl.ClientDaoImpl;
+import ru.kpfu.itis.mukminov.dao.impl.EmployeeDaoImpl;
+import ru.kpfu.itis.mukminov.service.ClientService;
+import ru.kpfu.itis.mukminov.service.EmployeeService;
+import ru.kpfu.itis.mukminov.service.impl.ClientServiceImpl;
+import ru.kpfu.itis.mukminov.service.impl.EmployeeServiceImpl;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    private static final String DATASOURCE_ATTR = "dataSource";
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            HikariConfig config = new HikariConfig("/db.properties");
+            HikariDataSource dataSource = new HikariDataSource(config);
+
+
+            sce.getServletContext().setAttribute(DATASOURCE_ATTR, dataSource);
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        HikariDataSource dataSource = (HikariDataSource) sce.getServletContext().getAttribute(DATASOURCE_ATTR);
+
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+}
